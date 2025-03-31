122d69a8837337076bfe8b36ca40c1cf477af1f9
Haim Hubara <haimhobara@gmail.com>
2025-04-01 00:57:51 +0300

    The same

--- src/5.4.communicating_vessels.py
+++ src/5.4.communicating_vessels.py
@@ -2,17 +2,30 @@ from itertools import zip_longest
 
 def interleave(*iterables):
     """
-    The func take cpuple iterators and uninte them one by one
+    The func takes a couple of iterators and unites them one by one.
 
     params: iterators
-    return: list of all the item from all the iterators one by one
-    
+    return: list of all the items from all the iterators one by one
     """
-    for items in zip_longest(*iterables, fillvalue=object()):
+    fill_value = object()  # יצירת אובייקט יחיד לפני הלולאה
+    for items in zip_longest(*iterables, fillvalue=fill_value):
         for item in items:
-            if item is not object():
+            if item is not fill_value:  # השוואה לאובייקט הקבוע
+                yield item
+
+def generator_interleave(*iterables):
+    """
+    The func takes a couple of iterators and unites them one by one, using the generator approach.
+
+    params: iterators
+    return: list of all the items from all the iterators one by one
+    """
+    fill_value = object()  # יצירת אובייקט יחיד לפני הלולאה
+    for items in zip_longest(*iterables, fillvalue=fill_value):
+        for item in items:
+            if item is not fill_value:  # השוואה לאובייקט הקבוע
                 yield item
 
 if __name__ == "__main__":
     result = list(interleave('abc', [1, 2, 3], ('!', '@', '#')))
-    print(result) 
+    print(result)
