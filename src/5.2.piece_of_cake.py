d62cfca8ba52ce17054a7c5827941da0db4591da
Haim Hubara <haimhobara@gmail.com>
2025-03-31 18:01:45 +0300

    fix the file 5.2.piece_of_cake.py

--- src/5.2.piece_of_cake.py
+++ src/5.2.piece_of_cake.py
@@ -1,21 +1,22 @@
 def piece_of_cake(dictionary, optional=None):
     """
-    The func calculate the price of the products
+    The func calculates the total price of products.
 
-    params: dictionary and  optional list
-    return: price off all the keys in the dictionary that not in optional list
+    :param dictionary: A dictionary where keys are prices and values are product names.
+    :param optional: A list of product names to exclude (default is None).
+    :return: The total price of all products not in the optional list.
     """
     if optional is None:
         optional = []
+    
     price_of_recipe = 0
-    for key, value in dictionary.items():
-        if value not in optional:
-            price_of_recipe += key
+    for price, product in dictionary.items():
+        if product not in optional: 
+            price_of_recipe += price  
 
     return price_of_recipe
 
 
-
 if __name__ == "__main__":
-    price = piece_of_cake({100:'tomato',300:'banana'},['tomato'])
-    print("price: " , price )
+    price = piece_of_cake({100: 'tomato', 300: 'banana'}, ['tomato',])
+    print("price:", price) 
