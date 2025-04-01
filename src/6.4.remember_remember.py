b817611a84a41107acdeb1ee31f4ad4747286dac
Haim Hubara <haimhobara@gmail.com>
2025-04-01 17:45:50 +0300

    fix remember remeber

--- src/6.4.remember_remember.py
+++ src/6.4.remember_remember.py
@@ -10,25 +10,7 @@ Functions:
 from PIL import Image
 
 def remember_remember(image_path):
-    """
-    The function tries to extract a hidden message from an image by looking for black pixels.
-
-    This function loads an image from the given path, scans it pixel
-
-
-
-from PIL import Image
-
-def remember_remember(image_path):
-    """
-    The function tries to extract a hidden message from an image by looking for black pixels.
-
-    params:
-    - image_path (str): The path to the image.
-
-    return:
-    - (str): The hidden message extracted from the image.
-    """
+    """The function tries to extract a hidden message from an image by looking for black pixels."""
     try:
         img = Image.open(image_path).convert("RGB")
     except Exception as e:
@@ -40,24 +22,12 @@ def remember_remember(image_path):
     message = []
 
     def is_black(pixel):
-        """ 
-        Checks if a pixel is mostly black
-
-        params: 
-        - pixel
-
-        return:
-        - true if is black else false
-
-        """
+        """Checks if a pixel is mostly black."""
         return all(channel <= 50 for channel in pixel)
 
     for x in range(width):
         for y in range(height):
             if is_black(pixels[x, y]):
-                # Here, we expect the black pixel locations to correspond to certain character data
-                # For the purpose of this example, we will need a proper algorithm for decoding
-                # For now, it's just a placeholder message.
                 message.append("X")
 
     return "".join(message)
